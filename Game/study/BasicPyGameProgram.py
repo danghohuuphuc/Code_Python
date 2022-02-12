ce002fa4538c40af794d36e71374548d168c59fb
danghohuuphuc <danghohuuphuc@gmail.com>
2022-02-12 09:34:49 +0700

    main

--- Game/study/BasicPyGameProgram.py
+++ Game/study/BasicPyGameProgram.py
@@ -13,5 +13,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
+    # draw a soild blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    
+    pygame.display.flip()
+pygame.quit()
